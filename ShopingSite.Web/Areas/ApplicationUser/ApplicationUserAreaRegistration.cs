57e12774326dcb45c257530a81d398816f8db38f
prashish shrestha <prashish.shrestha@apexcollege.edu.np>
2021-01-21 21:54:04 +0545

    Admin page added.

---
+++ ShopingSite.Web/Areas/ApplicationUser/ApplicationUserAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace ShopingSite.Web.Areas.ApplicationUser
+{
+    public class ApplicationUserAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "ApplicationUser";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "ApplicationUser_default",
+                "ApplicationUser/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ ShopingSite.Web/Areas/ApplicationUser/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShopingSite.Web.Areas.ApplicationUser.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: ApplicationUser/User
+        public async Task<ActionResult> Index()
+        {
+            return View();
+        }
+        public async Task<ActionResult> AdminIndex()
+        {
+            return View();
+        }
+    }
+}
