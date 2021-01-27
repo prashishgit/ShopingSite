41498c67e06906caddcb81e3eeacc6d889cee541
prashish shrestha <prashish.shrestha@apexcollege.edu.np>
2021-01-27 21:23:37 +0545

    table added in category menu

---
+++ ShopingSite.Web/Areas/Item/Controllers/CategoryController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShopingSite.Web.Areas.Item.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Item/Category
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ShopingSite.Web/Areas/Item/ItemAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace ShopingSite.Web.Areas.Item
+{
+    public class ItemAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Item";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Item_default",
+                "Item/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
