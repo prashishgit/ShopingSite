785eafb00ab246930ac8d7ab5c41cf12a25ee1fc
prashish shrestha <prashish.shrestha@apexcollege.edu.np>
2021-02-27 19:34:55 +0545

    errors commented in console in layoutAdmin. Added datatable.js file.

--- ShopingSite.Web/Areas/Item/Controllers/CategoryController.cs
+++ ShopingSite.Web/Areas/Item/Controllers/CategoryController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -10,6 +11,10 @@ namespace ShopingSite.Web.Areas.Item.Controllers
     {
         // GET: Item/Category
         public ActionResult Index()
+        {
+            return View();
+        }
+        public async Task<ActionResult> Create()
         {
             return View();
         }
---
+++ ShopingSite.Web/Areas/Item/Scripts/Category/category.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(function () {
+    //var mainBody = $('.content-wrapper');
+    //mainBody.find("#create").on('click', create);
+    // Attach Button click event listener 
+    $("#create-btn").click(function () {
+        debugger
+        // show Modal
+        $('.panel-green').modal('show');
+    });
+}); 
