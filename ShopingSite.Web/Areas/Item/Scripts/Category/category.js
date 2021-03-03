42a98fa78bed76a3be856b837837eb5b03ba34df
prashish shrestha <prashish.shrestha@apexcollege.edu.np>
2021-03-03 20:20:46 +0545

    modal pop up done in category create

--- ShopingSite.Web/Areas/Item/Scripts/Category/category.js
+++ ShopingSite.Web/Areas/Item/Scripts/Category/category.js
@@ -2,9 +2,13 @@
     //var mainBody = $('.content-wrapper');
     //mainBody.find("#create").on('click', create);
     // Attach Button click event listener 
-    $("#create-btn").click(function () {
-        debugger
-        // show Modal
-        $('.panel-green').modal('show');
-    });
+    var placeHolderElement = $('#placeHolderElement');
+    $('.btn-success[data-toggle="ajax-modal"]').click(function () {
+        var url = $(this).data('url');
+        $.get(url).done(function (data) {
+            placeHolderElement.html(data);
+            placeHolderElement.find('.modal').modal('show');
+        })
+    })
+
 }); 
