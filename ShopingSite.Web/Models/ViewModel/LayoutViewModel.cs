179a28bff51ee24a3a70d96eebfe17ed3b565c00
prashish shrestha <prashish.shrestha@apexcollege.edu.np>
2021-04-24 17:48:54 +0545

    dynamically get category in the main layout.

---
+++ ShopingSite.Web/Models/ViewModel/LayoutViewModel.cs
@@ -0,0 +1,18 @@
+﻿using ShopingSite.Web.Areas.Item.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShopingSite.Web.Models.ViewModel
+{
+    public class LayoutViewModel
+    {
+        public LayoutViewModel() 
+        {
+            this.CategoryListVM = new List<CategoryViewModel>();
+        }
+        public List<CategoryViewModel> CategoryListVM { get; set; }
+    }
+    
+}
